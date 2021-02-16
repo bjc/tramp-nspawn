c98260130b4608cf4f16c4c6dcb4edc0d494c69a
Brian Cully <bjc@kublai.com>
2021-02-16 17:29:41 -0500

    Add ‘Maintainer’ pseudo-keyword to header.

--- nspawn-tramp.el
+++ nspawn-tramp.el
@@ -5,6 +5,7 @@
 ;; Author: Brian Cully <bjc@kublai.com>
 ;; URL: https://github.com/bjc/nspawn-tramp
 ;; Keywords: tramp, nspawn, machinectl, systemd, systemd-nspawn
+;; Maintainer: Brian Cully <bjc@kublai.com>
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "23"))
 
